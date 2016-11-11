c7bb2e33127a24c7c2bc03ad4000fd363390aa6a
Developer-MINO <mino.kim.1st@gmail.com>
2016-11-11 12:48:33 +0900

    Initial Commit

---
+++ UISample/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  UISample
+//
+//  Created by mino on 2016. 11. 11..
+//  Copyright © 2016년 mino. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
